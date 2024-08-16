c43cac57c68451fcf958b0fe984feaebf5a2ae8e
tookender <ender.yeeterson@gmail.com>
2024-08-17 00:48:49 +0200

    ✨ Convert to Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] });
+
+export const metadata: Metadata = {
+  title: "susg.us",
+  description: "wip",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import "./page.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>
+          susg.us
+        </h1>
+        <p>just here for the funnies</p>
+      </div>
+    </main>
+  );
+}
